924b25d253b9eb06a90abf094ff4d5bde41df127
matinntb <ladynegar92@gmail.com>
2020-01-10 03:08:51 +0330

    update

--- soale 2 asli.py
+++ soale 2 asli.py
@@ -1,20 +1,19 @@
-from numpy import*
 import numpy as np
 n=int(input("Enter number of row:"))
 m=int(input("Enter number of column:"))
 k=int(input("Enter number of BOMB:"))
 
 if 1<n<100 and 1<m<100 and 0<k<m*n:
-    a=np.full((n,m),'0')
+    a=np.full((n,m),'0')        #creating array n*m
     
     for x in range(0,k):
         print("BOMBE ",x+1)
         x=int(input("Enter row of BOMB:"))
         y=int(input("Enter column of BOMB:"))
-        a[x-1][y-1]='*'
+        a[x-1][y-1]='*'         #set star in a[x-1][y-1]
 
     
-        if a[x-2][y-2]!='*':
+        if a[x-2][y-2]!='*':    #Insert corresponding numbers in adjacent cell 
                         f=int(a[x-2][y-2])
                         
                         f+=1
@@ -58,10 +57,6 @@ if 1<n<100 and 1<m<100 and 0<k<m*n:
                         f+=1
                         str(f)
                         a[x-1][y]=f
-                    
-                    
-                    
-                    
-                    
+                         
 print(a)
 
