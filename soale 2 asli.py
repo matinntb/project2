4c53f58ee11a31b06c8d46ced79aa09c778e350b
matinntb <59712378+matinntb@users.noreply.github.com>
2020-01-10 02:45:37 +0330

    Add files via upload

---
+++ soale 2 asli.py
@@ -0,0 +1,67 @@
+from numpy import*
+import numpy as np
+n=int(input("Enter number of row:"))
+m=int(input("Enter number of column:"))
+k=int(input("Enter number of BOMB:"))
+
+if 1<n<100 and 1<m<100 and 0<k<m*n:
+    a=np.full((n,m),'0')
+    
+    for x in range(0,k):
+        print("BOMBE ",x+1)
+        x=int(input("Enter row of BOMB:"))
+        y=int(input("Enter column of BOMB:"))
+        a[x-1][y-1]='*'
+
+    
+        if a[x-2][y-2]!='*':
+                        f=int(a[x-2][y-2])
+                        
+                        f+=1
+                        str(f)
+                        a[x-2][y-2]=f
+        if  a[x-2][y]!='*':
+                        f=int(a[x-2][y])
+                        f+=1
+                        str(f)
+                        a[x-2][y]=f
+        if  a[x-2][y-1]!='*':
+                        f=int(a[x-2][y-1])
+                        f+=1
+                        str(f)
+                        a[x-2][y-1]=f
+
+        if  a[x][y-2]!='*':
+                        f=int(a[x][y-2])
+                        f+=1
+                        str(f)
+                        a[x][y-2]=f
+
+        if a[x][y-1]!='*':
+                        f=int(a[x][y-1])
+                        f+=1
+                        str(f)
+                        a[x][y-1]=f
+        if a[x][y]!='*':
+                        f=int(a[x][y])
+                        f+=1
+                        str(f)
+                        a[x][y]=f
+
+        if  a[x-1][y-2]!='*':
+                        f=int(a[x-1][y-2])
+                        f+=1
+                        str(f)
+                        a[x-1][y-2]=f
+        if  a[x-1][y]!='*':
+                        f=int(a[x-1][y])
+                        f+=1
+                        str(f)
+                        a[x-1][y]=f
+                    
+                    
+                    
+                    
+                    
+print(a)
+
